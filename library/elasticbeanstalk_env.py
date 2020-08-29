b778c2e13250e8f848b343be058fb18fd73af7d3
Muthukumar S K <muthusk@juniper.net>
2020-08-29 07:40:12 +0000

    return option['Value'] only if Value is in option

--- library/elasticbeanstalk_env.py
+++ library/elasticbeanstalk_env.py
@@ -249,7 +249,8 @@ def new_or_changed_option(options, setting):
                 option["Value"] == setting["Value"]:
                 return None
             else:
-                return (option["Namespace"] + ':' + option["OptionName"], option["Value"], setting["Value"])
+                if 'Value' in option:
+                    return (option["Namespace"] + ':' + option["OptionName"], option["Value"], setting["Value"])
 
     return (setting["Namespace"] + ':' + setting["OptionName"], "<NEW>", setting["Value"])
 
